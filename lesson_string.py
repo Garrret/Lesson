181fd0dd0651e1652de8ca588b23c133fda43207
Garrret <perehodcev.igor@yandex.ru>
2024-08-21 17:39:26 +0700

    Add files via upload

---
+++ lesson_string.py
@@ -0,0 +1,12 @@
+stroka = str(input('Введите строку: '))
+# 1
+print(stroka[0])
+# 2
+print(stroka[-1])
+# 3
+dlina = len(stroka)
+print(stroka[dlina//2:])
+# 4
+print(stroka[::-1])
+# 5
+print(stroka[1::2])
